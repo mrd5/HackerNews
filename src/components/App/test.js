5b108fd1cda822f1bf0975c72501981ac8868b8d
Mathew Dorish <mathewdorish@hotmail.com>
2018-09-05 15:45:50 -0600

    Broke up the testing into four files so they're not all in App

--- src/components/App/test.js
+++ src/components/App/test.js
@@ -5,8 +5,6 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App, { Search, Button, Table } from './index.js';
 
-Enzyme.configure({ adapter: new Adapter() });
-
 //App component has two test
 describe('App', () => {
 
@@ -29,72 +27,4 @@ describe('App', () => {
 		expect(tree).toMatchSnapshot();
 	})
 
-});
-
-//Search component also has two tests
-describe('Search', () => {
-
-	//First test renders component to DOM, verifies there are no errors
-	it('Renders without crashing', () => {
-		const div = document.createElement('div');
-		ReactDOM.render(<Search>Search</Search>, div);
-		ReactDOM.unmountComponentAtNode(div);
-	});
-
-	//Stores snapshot, compares with previous snapshot
-	test('Has a valid snapshot', () => {
-		const component = renderer.create(
-			<Search>Search</Search>
-		);
-
-		let tree = component.toJSON();
-		expect(tree).toMatchSnapshot();
-	})
-});
-
-//Button component tests
-describe('Button', () => {
-	it('Renders without crashing', () => {
-		const div = document.createElement('div');
-		ReactDOM.render(<Button>Button</Button>, div);
-		ReactDOM.unmountComponentAtNode(div);
-	});
-
-	test('Has a valid snapshot', () => {
-		const component = renderer.create(
-			<Button>Button</Button>
-		);
-		let tree = component.toJSON();
-		expect(tree).toMatchSnapshot();
-	});
-});
-
-describe('Table', () => {
-	const props = {
-		list: [
-			{ title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
-			{ title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
-		],
-	};
-
-	it('Renders without crashing', () => {
-		const div = document.createElement('div');
-		ReactDOM.render(<Table{ ...props } />, div);
-	});
-
-	test('Has a valid snapshot', () => {
-		const component = renderer.create(
-			<Table { ...props } />
-		);
-		let tree = component.toJSON();
-		expect(tree).toMatchSnapshot();
-	})
-
-	it('Shows two items in the list', () => {
-		const element = shallow(
-			<Table { ...props } />
-		);
-
-		expect(element.find('.table-row').length).toBe(2);
-	})
 });
