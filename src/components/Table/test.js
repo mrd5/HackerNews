c6c0ee9faaab5395c18c2937401346a8df486598
Mathew Dorish <mathewdorish@hotmail.com>
2018-09-05 15:47:39 -0600

    Moved Table's testing to its own file

---
+++ src/components/Table/test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App, { Search, Button, Table } from '../App/index.js'; 
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Table', () => {
+	const props = {
+		list: [
+			{ title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+			{ title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+		],
+		sortKey: 'TITLE',
+		isSortedReverse: false,
+	};
+
+	it('Renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<Table{ ...props } />, div);
+	});
+
+	test('Has a valid snapshot', () => {
+		const component = renderer.create(
+			<Table { ...props } />
+		);
+		let tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	})
+
+	it('Shows two items in the list', () => {
+		const element = shallow(
+			<Table { ...props } />
+		);
+
+		expect(element.find('.table-row').length).toBe(2);
+	})
+});
